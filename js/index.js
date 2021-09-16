7e2af3b424b1a6297efa2e0f983492271c4e9892
Your Name <you@example.com>
2021-09-16 11:53:56 -0600

    modifying files

--- js/index.js
+++ js/index.js
@@ -32,13 +32,39 @@ function fetchDogs(num){
 
 
 }
+// alternative method to fetch info//
+async function fetchinInfo(num){
+    let url=`https://dog.ceo/api/breeds/image/random/${num}`;
+    let settings = {
+        method:"GET"
+    }
+
+        
+        let response= await fetch (url, settings);
+        let data= await response.json();
+
+
+        console.log(data);
+        let results=document.querySelector('.results');
+        results.innerHTML = "";
+
+        for(let i=0; i<data.message.length;i++){
+        let results=document.querySelector('.results');
+        results.innerHTML += `
+        <div>
+            <img src="${data.message[i]}" class="size">
+        </div>    
+             `;
+        }
+
+}
 
 function grabInfo (event){
     event.preventDefault();
 
     let currentNum= event.target.numOfDogs.value;
     console.log(currentNum);
-    fetchDogs(currentNum)
+    fetchinInfo(currentNum)
 }
 
 let dogForm=document.querySelector('#dogForm');
